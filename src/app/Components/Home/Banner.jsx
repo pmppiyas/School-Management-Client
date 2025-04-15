8169821ac2af2a65948706e9987f3889931cf49a
PmpPiyas <princemahmudpiyas@gmail.com>
2025-04-16 00:08:22 +0600

    Setup Commit

---
+++ src/app/Components/Home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div>
+      <h2>Banner </h2>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/app/Layouts/RootLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RootLayout() {
+  return (
+    <div>
+      <p>Root Layout</p>
+    </div>
+  );
+}
+
+export default RootLayout;
